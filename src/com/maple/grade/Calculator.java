070cb41d3f79d7710a7f3522e104bc604b42c8c6
JunhaKim <kimjh4930@nate.com>
2015-01-08 00:17:28 +0900

    파일 입력 기능 추가 완료

--- src/com/maple/grade/Calculator.java
+++ src/com/maple/grade/Calculator.java
@@ -1,10 +1,43 @@
 package com.maple.grade;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 public class Calculator {
 	
-	public String cal(){
-		System.out.println("hello world");
+	private FileInputStream fstream = null;
+	private String line = null;
+	private String number = null;
+	private String subjectName = null;
+	private String numberApiKey = null;
+	private String subjectNameApiKey = null;
+	
+	public String loadFileInfo() throws IOException {
+		
+		try{
+			File inFile = new File("gradeInfo.txt");
+			fstream = new FileInputStream(inFile);
+			DataInputStream in = new DataInputStream(fstream);
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			
+			//한 줄씩 불러들임
+			while((line = br.readLine()) != null && line != ""){
+				
+			}
+			
+			
+		}catch(FileNotFoundException ex){
+			Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
+			
+		}
 		
 		return null;
 	}
